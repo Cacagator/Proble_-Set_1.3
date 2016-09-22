87352f2137b85c839ba41954d8198d3c1cb2f5dd
Miao Sun <cactus@i21a-s1.ufhpc>
2016-09-22 13:40:34 -0400

    this script extracting the hightest temperature of each year in August

--- hottest_day_year.sh
+++ hottest_day_year.sh
@@ -7,7 +7,7 @@
 ###get each hottest temperature
 for year in {1973..2016} #For each year between 1943 and 2016
 do
-	Tmp=`grep "$year" Hottest_August_Days_1973-2016.txt|cut -f3|sort -n|tail -1`
-	echo -e "$year\t$Tmp" >>Temperature_Hottest_year.txt
+	Tmp=`grep "$year" Hottest_August_Days_1973-2016.txt|cut -f3|sort -n|tail -1` #for each year get the highest temperature among 31 days in August
+	echo -e "$year\t$Tmp" >>Temperature_Hottest_year.txt #output
 done
-sed -i 1'i\Year\tTemperature' Temperature_Hottest_year.txt
+sed -i 1'i\Year\tTemperature(F)' Temperature_Hottest_year.txt #adding header
